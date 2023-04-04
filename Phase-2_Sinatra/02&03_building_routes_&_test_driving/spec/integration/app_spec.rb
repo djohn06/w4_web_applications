b1d89bc5fb690127dad20424bcef77e09ead4017
djohn06 <danielajohn1406@gmail.com>
2023-04-04 11:20:25 +0100

    Created app and app_spec for application

---
+++ Phase-2_Sinatra/02&03_building_routes_&_test_driving/spec/integration/app_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+require 'rack/test'
+require_relative "../../app"
+
+describe Application do
+    include Rack::Test::Methods
+  
+    let(:app) { Application.new }
+  
+    context "GET /" do
+        it 'returns all names' do
+            response = get('/names')
+      
+            expect(response.status).to eq(200)
+            expect(response.body).to eq("Julia, Mary, Karim")
+        end
+    end
+
+    context "POST /" do
+        it "returns all names sorted alphabetically" do
+            response = post('/sort-names')
+
+            expect(response.status).to eq(200)
+            expect(response.body).to eq("Alice,Joe,Julia,Kieran,Zoe")
+        end
+    end
+end
